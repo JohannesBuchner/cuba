88043d9912f488f50a00771e09f9c9a19e177e4e
Johannes Buchner <johannes.buchner.acad@gmx.com>
2015-01-09 13:12:21 +0100

    new approach for creating libcuba.so: run makesharedlib.sh

---
+++ makesharedlib.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Author: Johannes Buchner (C) 2015
+# For creating a shared library (libcuba.so).
+
+sed 's/CFLAGS = -O3 -fomit-frame-pointer/CFLAGS = -O3 -fPIC -fomit-frame-pointer/g' --in-place makefile
+echo "rebuilding libcuba.a archive"
+make -B libcuba.a
+echo "unpacking libcuba.a"
+FILES=$(ar xv libcuba.a |sed 's/x - //g')
+echo "making libcuba.so"
+echo gcc -shared -Wall $FILES -o libcuba.so
+gcc -shared -Wall $FILES -o libcuba.so
+rm $FILES
+
+
