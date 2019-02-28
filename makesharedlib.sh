e329ffbc14456df7a58258fa04158acdc3defd22
María Benito <mariabenito@protonmail.com>
2019-02-28 17:26:03 -0300

    Update makesharedlib.sh
    I had a problem while building the cuba shared library, I got an error after typing "./makesharedlib.sh".
    The problem comes from the file __.SYMDEF SORTED which is not found.
    
    This is a pull request to grep out " __.SYMDEF SORTED__" as you suggested in issue #117 from pyMultinest.

--- makesharedlib.sh
+++ makesharedlib.sh
@@ -6,7 +6,7 @@ sed 's/CFLAGS = -O3 -fomit-frame-pointer/CFLAGS = -O3 -fPIC -fomit-frame-pointer
 echo "rebuilding libcuba.a archive"
 make -B libcuba.a
 echo "unpacking libcuba.a"
-FILES=$(ar xv libcuba.a |sed 's/x - //g')
+FILES=$(ar xv libcuba.a |sed 's/x - //g'|grep -v ' __\.SYMDEF SORTED__')
 echo "making libcuba.so"
 echo gcc -shared -Wall -lm $FILES -o libcuba.so
 gcc -shared -Wall -lm $FILES -o libcuba.so
