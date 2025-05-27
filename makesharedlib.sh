d3a5245be651f991d234e049b0ade60a980d4b6e
Johannes Buchner <johannes.buchner.acad@gmx.com>
2025-05-27 07:39:26 +0200

    Update makesharedlib.sh
    make it work again on Linux and MacOS
    
    see https://github.com/JohannesBuchner/cuba/issues/12#issuecomment-2910678781

--- makesharedlib.sh
+++ makesharedlib.sh
@@ -2,7 +2,7 @@
 # Author: Johannes Buchner (C) 2015
 # For creating a shared library (libcuba.so).
 
-sed 's/CFLAGS = -O3 -fomit-frame-pointer/CFLAGS = -O3 -fPIC -fomit-frame-pointer/g' --in-place makefile
+sed 's/CFLAGS = -O3 -fomit-frame-pointer/CFLAGS = -O3 -fPIC -fcommon -fomit-frame-pointer/g' --in-place makefile
 echo "rebuilding libcuba.a archive"
 make -B libcuba.a
 echo "unpacking libcuba.a"
